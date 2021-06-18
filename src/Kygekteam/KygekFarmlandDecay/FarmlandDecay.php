e7ede815da99a88c470ee7de6ae24d762d58d14c
Kygekraqmak <techfixindonesia@gmail.com>
2021-06-18 19:25:35 +0800

    Added code

---
+++ src/Kygekteam/KygekFarmlandDecay/FarmlandDecay.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * Implements vanilla functionality of farmland becomes dirt when jumped or fallen
+ * Copyright (C) 2021 KygekTeam
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+declare(strict_types=1);
+
+namespace KygekTeam\KygekFarmlandDecay;
+
+use KygekTeam\KtpmplCfs\KtpmplCfs;
+use pocketmine\block\Block;
+use pocketmine\block\BlockFactory;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\plugin\PluginBase;
+
+class FarmlandDecay extends PluginBase implements Listener {
+
+    public function onEnable() {
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        KtpmplCfs::checkUpdates($this);
+    }
+
+    public function onMove(PlayerMoveEvent $event) {
+        $level = $event->getPlayer()->getLevel();
+
+        // Checks if block below the destination is farmland
+        if ($level->getBlock($block = $event->getTo()->subtract(0, 1, 0))->getId() !== Block::FARMLAND) return;
+        // Must be 0.5 to work properly
+        if (!(($event->getFrom()->getY() - 0.5) >= $event->getTo()->getY())) return;
+        if ($event->getPlayer()->isFlying()) return;
+
+        $level->setBlock($block, BlockFactory::get(Block::DIRT));
+    }
+
+}
